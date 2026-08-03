96b4352e8eee25d4c82d4c2f65b607221c9b4635
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2026-08-02 22:59:01 -0600

    Landing navbar fixed

--- components/layout/navbars/landingnav.tsx
+++ components/layout/navbars/landingnav.tsx
@@ -19,11 +19,12 @@
             <Image
               src={"/LogosValtrust/valtrust-isologo.png"}
               alt="Logo Image"
-              width={125}
-              height={125}
+              width={170}
+              height={170}
             
             />
             </Link>
+            
           </div>
 
           <div className="md:block hidden w-2/3 md:text-md">
@@ -46,7 +47,21 @@
             </ul>
           </div>
 
-          <div className="flex flex-col w-full justify-end md:hidden ">
+          <div className="flex md:hidden p-2 w-full items-center justify-center gap-5 text-sm md:text-sm" >
+          <button className="p-3 bg-zinc-200 w-1/2 rounded-lg font-bold">
+          <Link href={"/login"}>
+            Sign In
+            </Link>
+          </button>
+          <button className="p-3 bg-bl-main  w-1/2 rounded-lg text-white font-bold" onClick={onSignUpClick}>
+          
+
+            Sign Up
+          
+          </button>
+          </div>
+
+          <div className="flex flex-col w-1/3 justify-end md:hidden ">
             <button
               className="text-3xl md:hidden w-full justify-end items-center p-5 flex items-center flex-3z-50 z-20"
               onClick={() => setOpen(!open)}
@@ -80,7 +95,7 @@
           <div className= {`  ${open ? "" : "translate-x-full"
 
           } transform transition-transform duration-300 ease-in-out flex text-black flex-col md:hidden w-50 p-10 fixed bg-wh-main items-center  h-screen top-0 right-0
-           gap-20 text-xl z-10  rounded-lg shadow-lg shadow-5xl border-white mt-20`}>
+           gap-20 text-xl z-10   shadow-lg  border-white mt-20`}>
             <div className="flex flex-col items-center text-left gap-8 ">
               
               <div className="w-full ">
@@ -124,19 +139,7 @@
       
 
 
-        <div className="flex md:hidden w-full items-center justify-center p-3 gap-5 text-lg md:text-sm">
-          <button className="p-5 bg-zinc-200 w-1/3 rounded-lg font-bold">
-          <Link href={"/login"}>
-            Sign In
-            </Link>
-          </button>
-          <button className="p-5 bg-bl-main  w-1/3 rounded-lg text-white font-bold" onClick={onSignUpClick}>
-          
-
-            Sign Up
-          
-          </button>
-        </div>
+       
       </nav>
     );
   }
